1a6ca46a936e79815c89c892f0283fc64ed70fdd
jyo123-del <72658081+jyo123-del@users.noreply.github.com>
2020-10-31 16:45:06 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class Box{
+    constructor(){
+        var options= {
+            restitution: 1
+        }
+        this.body=Bodies.rectangle(200,200,50,50,options);
+        this.width=50;
+        this.height=50;
+        World.add(world,this.body);
+        
+    }
+    display(){
+        var pos=this.body.position;
+        rectMode(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.width,this,height);
+    }
+}
